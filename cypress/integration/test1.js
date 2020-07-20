03e6c19c6999989fb40ece18ac1b8f13a88ffa7a
Andrei Ancas <andrei.ancas@bannersnack.com>
2020-07-20 11:33:33 +0300

    first commit

---
+++ cypress/integration/test1.js
@@ -0,0 +1,8 @@
+// Problema
+// 1. Mergi pe pagina https://app.bannersnack.com/auth/login/
+// 2. Incearca sa verifici minim 2 erori de login prin assert-uri
+// 3. Fa un login cu user si password valide user: andreibstest+interviu22@gmail.com pass: andreibstest+interviu22@gmail.com
+// 4. Verifica daca url-ul dupa login contine "/banner-creator/new/"
+// 5. Verifica daca butonul din Single design contine valoarea "Make a design"
+// 6. Mergi pe https://app.bannersnack.com/auth/logout/
+// 7. Gandeste-te la o modalitate prin care sa te asiguri ca vei fi delogat/a din aplicatie
---
+++ cypress/integration/test2.js
@@ -0,0 +1,28 @@
+// TODO: Problema 1
+
+// 1. Declara un array cu 3 obiecte in el continand name,age.
+//  Primul va avea valoarea name - Andrei age - 28
+//  Al 2-lea va avea valoarea name - Levi age - 31
+//  Al 3-lea va avea valoarea name - Laviniu age - 50
+// 2. Creeaza o functie care iti returneaza un string invers (de la ultima litera la prima)
+// 3. Creeaza o alta functie care verifica daca varsta e mai mare sau egala cu 30
+//  si returneaza valoare boolean in functie de rezultatul dorit
+// 4. Trebuie sa parcurgi array-ul si pentru fiecare obiect care indeplineste varsta specificata mai sus
+//  vei apela functia creata mai sus (cea de inversare string)
+//  pe valoarea numelui si vei afisa in consola rezultatul final
+
+
+// TODO: Problema 2
+
+// 1. Extrage din array-ul de mai jos doar numerele care contin cifra 3
+const arr = [1, 43, 57, 23, 89, 56, 23, 53, 84, 95, 83, 34];
+// 2. Declara un nou array care va contine doar cifrele primite de la punctul 1
+// 3. Scoate prima si ultima pozitie din array si returneaza in consola valorile din array
+
+
+// TODO: Problema 3
+
+// 1. Adauga textul 'orasul ' la string-ul de mai jos 
+const str = 'Oradea'
+// 2. Avand noul string de mai sus complet returneaza in consola
+//  acealasi string dar cu prima litera uppercase
